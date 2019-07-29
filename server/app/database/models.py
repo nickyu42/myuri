feecd694789348882a52730ee641640624fc1692
Nick <nickyu42@gmail.com>
2019-07-29 11:42:52 -0300

    Change model metaclass to mixin
    Add script for inserting data into db

--- server/app/database/models.py
+++ server/app/database/models.py
@@ -4,12 +4,13 @@ Date created: 23/7/2019
 """
 import enum
 from typing import Type
+from sqlalchemy.ext.declarative import declared_attr
 
 from app.database.db import db
 
 
 class BaseMixin:
-    """Mixin class for extra functionality"""
+    """Mixin class for extra base functionality"""
 
     @staticmethod
     def _create(cls: Type[db.Model], *args, **kwargs):
@@ -22,6 +23,10 @@ class BaseMixin:
         """Create an instance of this Model"""
         cls._create(cls, *args, **kwargs)
 
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
     def __repr__(self: db.Model):
         return f'<{type(self).__name__}>'
 
@@ -31,8 +36,28 @@ class KeyMixin(BaseMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     def __repr__(self):
-        columns = [f'{key}={val}' for key, val in vars(self).items() if not key.startswith('_')]
+        columns = [f'{key}={val if len(str(val)) < 10 else "..."}' for key, val
+                   in vars(self).items()
+                   if not key.startswith('_')]
         return f'<{type(self).__name__} {" ".join(columns)}>'
+
+
+class ComicDataMixin(KeyMixin):
+    """Mixin class that that has a many to many relationship with Comic"""
+    val = db.Column(db.String(80), unique=True, nullable=False)
+
+    @declared_attr
+    def comic_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('comic.id'), nullable=False)
+
+    @declared_attr
+    def table(cls):
+        name = cls.__tablename__
+        return db.Table(
+            f'{name}_comic',
+            db.Column(f'{name}_id', db.Integer, db.ForeignKey(f'{name}.id'), primary_key=True),
+            db.Column('comic_id', db.Integer, db.ForeignKey('comic.id'), primary_key=True)
+        )
 
 
 class ComicType(enum.Enum):
@@ -43,16 +68,25 @@ class ComicType(enum.Enum):
     Western = 4
 
 
-class Comic(KeyMixin, db.Model):
-    type = db.Column(db.Enum(ComicType), nullable=False)
-    total_chapters = db.Column(db.Integer, nullable=True)
-    total_volumes = db.Column(db.Integer, nullable=True)
-    description = db.Column(db.Text, nullable=True)
+class ComicFormat(enum.Enum):
+    """Enum for comic formats"""
+    LeftToRight = 1
+    RightToLeft = 2
+    Webtoon = 3
 
 
 class Chapter(KeyMixin, db.Model):
+    number = db.Column(db.Integer, nullable=False)
     total_pages = db.Column(db.Integer, nullable=False)
     comic_id = db.Column(db.Integer, db.ForeignKey('comic.id'), nullable=False)
+    volume_id = db.Column(db.Integer, db.ForeignKey('volume.id'), nullable=True)
+
+
+class Volume(KeyMixin, db.Model):
+    number = db.Column(db.Integer, nullable=False)
+    comic_id = db.Column(db.Integer, db.ForeignKey('comic.id'), nullable=False)
+
+    chapters = db.relationship('Chapter', lazy=True, backref=db.backref('Volume', lazy=True))
 
 
 class ComicName(KeyMixin, db.Model):
@@ -60,32 +94,40 @@ class ComicName(KeyMixin, db.Model):
     comic_id = db.Column(db.Integer, db.ForeignKey('comic.id'), nullable=False)
 
 
-def create_data_class(name: str) -> Type:
-    """
-    Creates a metaclass that represents a table
-    with a many-to-many relationship with Comic
-    :param name: name to give the table
-    :return: Class object
-    """
-    return type(
-        name,
-        (KeyMixin, db.Model),
-        {
-            '__tablename__': name,
-            'val': db.Column(db.String(80), unique=True, nullable=False),
-            'comic_id': db.Column(db.Integer, db.ForeignKey('comic.id'), nullable=False),
-            'table': db.Table(
-                f'{name}_comic',
-                db.Column(f'{name}_id', db.Integer, db.ForeignKey(f'{name}.id'), primary_key=True),
-                db.Column('comic_id', db.Integer, db.ForeignKey('comic.id'), primary_key=True)
-            )
-        }
-    )
+class Tag(ComicDataMixin, db.Model):
+    """Comic tag table"""
 
 
-Tag = create_data_class('tag')
-Artist = create_data_class('artist')
-Author = create_data_class('author')
-Genre = create_data_class('genre')
+class Artist(ComicDataMixin, db.Model):
+    """Comic artist table"""
 
 
+class Author(ComicDataMixin, db.Model):
+    """Comic author table"""
+
+
+class Genre(ComicDataMixin, db.Model):
+    """Comic genre table"""
+
+
+class Comic(KeyMixin, db.Model):
+    type = db.Column(db.Enum(ComicType), nullable=True)
+    total_chapters = db.Column(db.Integer, nullable=True)
+    total_volumes = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    format = db.Column(db.Enum(ComicFormat), nullable=True)
+
+    names = db.relationship('ComicName', lazy='joined', backref=db.backref('comic', lazy='joined'))
+
+    # a comic is loaded without chapters/volume
+    # while in reverse direction the loading is joined because
+    # usually the chapter/volume is required with the corresponding Comic metadata
+    chapters = db.relationship('Chapter', lazy=True, backref=db.backref('comic', lazy='joined'))
+    volumes = db.relationship('Volume', lazy=True, backref=db.backref('comic', lazy='joined'))
+
+    # metadata is loaded as necessary
+    # chapters are usually filtered based on metadata, so subquery is used
+    tags = db.relationship('Tag', lazy=True, secondary=Tag.table, backref=db.backref('comics', lazy='dynamic'))
+    artists = db.relationship('Artist', lazy=True, secondary=Artist.table, backref=db.backref('comics', lazy='dynamic'))
+    authors = db.relationship('Author', lazy=True, secondary=Author.table, backref=db.backref('comics', lazy='dynamic'))
+    genres = db.relationship('Genre', lazy=True, secondary=Genre.table, backref=db.backref('comics', lazy='dynamic'))
--- server/app/database/utils.py
+++ server/app/database/utils.py
@@ -4,11 +4,27 @@ Date created: 23/7/2019
 """
 import click
 import os
+from typing import List, Optional
 from flask.cli import AppGroup
 
 from app.database.models import *
 
 db_cli = AppGroup('db')
+
+
+def get_model(name: str) -> Optional[db.Model]:
+    """
+    Get Model if it exists
+    :param name: name of the table to find
+    :return: Type object of db.Model
+    """
+    name = name.lower()
+
+    if name in db.metadata.tables:
+        gen = (v for k, v in db.Model._decl_class_registry.items() if k.lower() == name)
+        return next(gen, None)
+
+    return None
 
 
 @db_cli.command('reset')
@@ -69,14 +85,29 @@ def view(table: str):
     Prints given objects in 'table'
     :param table: name of the table to view
     """
-    table = table.lower()
+    cls: db.Model = get_model(table)
 
-    if table in db.metadata.tables:
-        gen = (v for k, v in db.Model._decl_class_registry.items() if k.lower() == table)
-        cls: db.Model = next(gen, None)
+    if cls:
+        for item in cls.query.limit(5):
+            print(item)
+    else:
+        print(f'{table} does not exist')
 
-        if cls:
-            for item in cls.query.limit(5):
-                print(item)
+
+@db_cli.command('insert')
+@click.argument('table')
+@click.argument('args', nargs=-1)
+def insert(table: str, args: List[str]):
+    """
+    Inserts given object into table
+    Horribly hacky code!, should only ever be allowed for testing
+    :param table: which table to insert to
+    :param args: arguments, should be formatted as "<KEY>=<VAL>"
+    """
+    cls: BaseMixin = get_model(table)
+
+    if cls:
+        kwargs = {k: eval(v) for k, v in [tuple(a.split('=')) for a in args]}
+        cls.create(**kwargs)
     else:
         print(f'{table} does not exist')
