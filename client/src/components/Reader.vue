7f3b8362384a011f159104a3e0e4bb752d952953
Nick Yu <n.j.k.yu@student.tudelft.nl>
2020-03-13 11:48:42 +0100

    Add page in Reader.vue

--- client/src/components/Reader.vue
+++ client/src/components/Reader.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="reader">
+
     <v-app-bar
         hide-on-scroll="true"
         dark
@@ -7,30 +8,54 @@
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
   
         <v-toolbar-title>Myuri</v-toolbar-title>
-      </v-app-bar>
-  
-      <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-      >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group
-            v-model="group"
-          >
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item>
 
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-container>
+      <v-row
+        justify="center"
+      >
+        <v-img
+          src=""
+          max-width="700"
+        >
+
+        </v-img>
+      </v-row>
+
+      <v-row>
+        <v-pagination></v-pagination>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
